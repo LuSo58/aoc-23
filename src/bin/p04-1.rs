9f61a0a15b362213e9c85f8881668c39f3f86220
Martin Horský <martin@horsky.me>
2023-12-04 20:37:47 +0100

    4.12.

---
+++ src/bin/p04-1.rs
@@ -0,0 +1,41 @@
+use std::collections::HashSet;
+use std::io::stdin;
+
+fn main() {
+    let sum = stdin()
+        .lines()
+        .map(|line| {
+            line.map(|line| {
+                match line.split_once(':') {
+                    Some((_, numbers)) => {
+                        match numbers.split_once('|') {
+                            Some((winning_numbers, betting_numbers)) => {
+                                let betting_numbers = betting_numbers
+                                    .split(' ')
+                                    .filter_map(|number| {
+                                        number.parse::<u32>().ok()
+                                    })
+                                    .collect::<HashSet<_>>();
+                                let numbers_won = winning_numbers
+                                    .split(' ')
+                                    .filter_map(|number| {
+                                        number.parse::<u32>().ok()
+                                    })
+                                    .filter(|number| {
+                                        betting_numbers.contains(number)
+                                    }).count();
+                                match numbers_won {
+                                    0 => 0,
+                                    1.. => 1 << (numbers_won - 1),
+                                    _ => panic!("Impossible: negative usize")
+                                }
+                            }
+                            None => panic!("Bad input: pipe")
+                        }
+                    }
+                    None => panic!("Bad input: semicolon")
+                }
+            }).expect("Failed reading from input")
+        }).sum::<u32>();
+    println!("{sum}");
+}
---
+++ src/bin/p04-2.rs
@@ -0,0 +1,46 @@
+use std::collections::{HashSet, VecDeque};
+use std::io::stdin;
+use std::iter::repeat;
+
+fn main() {
+    let sum = stdin()
+        .lines()
+        .map(|line| {
+            line.map(|line| {
+                match line.split_once(':') {
+                    Some((_, numbers)) => {
+                        match numbers.split_once('|') {
+                            Some((winning_numbers, betting_numbers)) => {
+                                let betting_numbers = betting_numbers
+                                    .split(' ')
+                                    .filter_map(|number| {
+                                        number.parse::<u32>().ok()
+                                    })
+                                    .collect::<HashSet<_>>();
+                                winning_numbers
+                                    .split(' ')
+                                    .filter_map(|number| {
+                                        number.parse::<u32>().ok()
+                                    })
+                                    .filter(|number| {
+                                        betting_numbers.contains(number)
+                                    }).count()
+                            }
+                            None => panic!("Bad input: pipe")
+                        }
+                    }
+                    None => panic!("Bad input: semicolon")
+                }
+            }).expect("Failed reading from input")
+        })
+        .fold((0, VecDeque::new()), |(total_cards, mut forward_winnings), numbers_won| {
+            let cards = forward_winnings.pop_front().unwrap_or(0usize) + 1;
+            forward_winnings.extend(repeat(0).take(numbers_won.saturating_sub(forward_winnings.len())));
+            forward_winnings
+                .iter_mut()
+                .take(numbers_won)
+                .for_each(|forward_duplicate| *forward_duplicate += cards);
+            (total_cards + cards, forward_winnings)
+        }).0;
+    println!("{sum}");
+}
